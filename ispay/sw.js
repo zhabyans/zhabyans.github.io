1f77f6f3bb12ad24cb721ffe34ec8a177f50cd21
Muhammad Zhafran Zhabyansyah <zhafranku@gmail.com>
2025-09-23 11:56:29 +0700

    Add files via upload

---
+++ ispay/sw.js
@@ -0,0 +1,44 @@
+// file: sw.js
+const CACHE_NAME = "data-cache-v43";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./dist/bundle.js",
+  "./style.css",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
